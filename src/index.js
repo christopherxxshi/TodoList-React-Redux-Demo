f0c73044de1b25e9593a3092ef8dea967ff3b553
Chris Shi <uschrisshi@gmail.com>
2019-02-12 10:41:39 -0500

    added selectors

--- src/index.js
+++ src/index.js
@@ -6,9 +6,11 @@ import { Provider } from "react-redux";
 import thunkMiddleware from "redux-thunk";
 import rootReducer from "./reducers";
 
-
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunkMiddleware)));
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunkMiddleware))
+);
 
 ReactDOM.render(
   <Provider store={store}>
